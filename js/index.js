4e8886ed9744254c30d06c47b5ed4aa78b866a8d
ivan perez <ivan_perez0980@proton.me>
2023-09-11 17:23:40 -0300

    proyecto humaya terminado con cosnumo de api incluido

--- js/index.js
+++ js/index.js
@@ -13,3 +13,61 @@ navWrapper.addEventListener('click',e => {
     toggleButton.classList.remove('close')
   }
 })
+
+
+/*AQUI VA EL CONSUMO DE LA API */
+
+/*URL DE LA API*/
+const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=3&q=caramel%20sauce';
+
+
+/* API KEY Y METODO GET  */
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '19be94db4cmshcca437cbcbdb5f0p108358jsn7331bebed3c0',
+        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+    }
+};
+
+
+const cargarPostres = async () => {
+
+  try {
+      const respuesta = await fetch(url, options);
+
+      if (respuesta.status == 200) {
+          const data = await respuesta.json();
+
+          let postres = "";
+
+          //  const { name, description, thumbnail_url } =data.results;
+
+          data.results.forEach(({ name, description, thumbnail_url }) => {
+                  
+              postres +=`
+              <div class="card-recetas">
+              <img class="card-img-top" src="${thumbnail_url}" alt="Card image cap">
+              <div class="card-body-recetas">
+                  <h5 class="card-title-recetas">${name}</h5>
+                  <p class="card-text-recetas">${description}</p>
+                  <a class="btn-recetas" href="#">Preparación</a>
+              </div>
+          </div>
+              ` 
+          })
+
+
+          document.getElementById("card-deck").innerHTML = peliculas;
+
+
+
+      }
+
+
+  } catch (error) {
+      console.log(error.message);
+  }
+}
+
+cargarPostres();
